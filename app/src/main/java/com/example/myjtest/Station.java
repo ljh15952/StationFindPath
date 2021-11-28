7dde8ac3c04ef072b49e68b78330b30e7bbfec2c
Can I? <ljh15952@gmail.com>
2021-11-28 15:37:54 +0900

    v1

--- app/src/main/java/com/example/myjtest/Station.java
+++ app/src/main/java/com/example/myjtest/Station.java
@@ -1,9 +1,10 @@
 package com.example.myjtest;
 
 public class Station {
-    private int x1,y1,x2,y2;
-    private int name;
-    public Station(int name,int x1,int y1,int x2,int y2){
+    private int x1, y1, x2, y2;
+    public int name;
+
+    public Station(int name, int x1, int y1, int x2, int y2) {
         this.name = name;
         this.x1 = x1;
         this.y1 = y1;
@@ -11,13 +12,13 @@ public class Station {
         this.y2 = y2;
     }
 
-    public boolean isClicked(int x,int y){
-        if((x > x1) && (x < x2) && (y > y1) && (y < y2))
+    public boolean isClicked(int x, int y) {
+        if ((x > x1) && (x < x2) && (y > y1) && (y < y2))
             return true;
         return false;
     }
 
-    public int getName(){
+    public int getName() {
         return name;
     }
 }
--- app/src/main/java/com/example/myjtest/pathViewActivity.java
+++ app/src/main/java/com/example/myjtest/pathViewActivity.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -20,9 +19,13 @@ import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+import java.util.Stack;
+
 public class pathViewActivity extends AppCompatActivity {
     SubsamplingScaleImageView imageView;
     GestureDetector gestureDetector = null;
+    Stack<Station> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +33,25 @@ public class pathViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_path_view);
         imageView = findViewById(R.id.imageView);
         imageView.setImage(ImageSource.asset("map.png"));
+        list = new Stack<>();
         imageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 gestureDetector.onTouchEvent(motionEvent);
+                //if (list.size() > 0)
+                  //  Log.d("ASD", list.pop().name + "");
                 return false;
             }
         });
+
         gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() { // gesture 디텍팅으로 지하철 위치 읽기
             @Override
             public boolean onSingleTapUp(MotionEvent ev) {
+
                 if (imageView.isReady()) {
                     PointF sCoord = imageView.viewToSourceCoord(ev.getX(), ev.getY());
                     int x_cor = (int) sCoord.x;
                     int y_cor = (int) sCoord.y;
-                    Log.d("x", x_cor + "");
-                    Log.d("y", y_cor + "");
-
                     Response.Listener<String> responseListener = new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
@@ -59,10 +64,11 @@ public class pathViewActivity extends AppCompatActivity {
                                     int y1 = jsonObject.getInt("Y1");
                                     int x2 = jsonObject.getInt("X2");
                                     int y2 = jsonObject.getInt("Y2");
-
-                                    Station st = new Station(name,x1,y1,x2,y2);
-                                    if(st.isClicked(x_cor,y_cor))
+                                    Station st = new Station(name, x1, y1, x2, y2);
+                                    if (st.isClicked(x_cor, y_cor)) {
+                                        list.add(st);
                                         Toast.makeText(getApplicationContext(), name + "클릭!!", Toast.LENGTH_SHORT).show();
+                                    }
                                 }
                             } catch (JSONException e) {
                                 Toast.makeText(getApplicationContext(), "값 가져오기 실패", Toast.LENGTH_SHORT).show();
@@ -70,9 +76,28 @@ public class pathViewActivity extends AppCompatActivity {
                             }
                         }
                     };
-                    DataRequest loginRequest = new DataRequest(101 + "", responseListener);
-                    RequestQueue queue = Volley.newRequestQueue(pathViewActivity.this);
-                    queue.add(loginRequest);
+                    //
+                    for (int i = 101; i <= 123; i++) {
+                        DataRequest loginRequest = new DataRequest(i + "", responseListener);
+                        RequestQueue queue = Volley.newRequestQueue(pathViewActivity.this);
+                        queue.add(loginRequest);
+                    }
+                    for (int i = 201; i <= 217; i++) {
+                        DataRequest loginRequest = new DataRequest(i + "", responseListener);
+                        RequestQueue queue = Volley.newRequestQueue(pathViewActivity.this);
+                        queue.add(loginRequest);
+                    }
+                    for (int i = 301; i <= 308; i++) {
+                        DataRequest loginRequest = new DataRequest(i + "", responseListener);
+                        RequestQueue queue = Volley.newRequestQueue(pathViewActivity.this);
+                        queue.add(loginRequest);
+                    }
+                    for (int i = 401; i <= 417; i++) {
+                        DataRequest loginRequest = new DataRequest(i + "", responseListener);
+                        RequestQueue queue = Volley.newRequestQueue(pathViewActivity.this);
+                        queue.add(loginRequest);
+
+                    }
                 }
                 return super.onSingleTapUp(ev);
             }
